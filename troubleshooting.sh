0933cc0a65a1ab88d3c539b8fd6275d98f03c9e2
Alex Kigotho <129921136+Rastamonk01@users.noreply.github.com>
2026-04-29 13:55:44 +0930

    mister_a

---
+++ troubleshooting.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mkdir -p ~/troubleshoot-lab/docs
+mkdir -p ~/troubleshoot-lab/archive
+mkdir -p ~/troubleshoot-lab/logs
+touch ~/troubleshoot-lab/docs/report.txt
+echo "Linux troubleshooting practice file" > ~/troubleshoot-lab/docs/report.txt
+echo "second line of text" >> ~/troubleshoot-lab/docs/report.txt
